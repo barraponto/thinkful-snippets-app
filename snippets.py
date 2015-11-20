53d79ba3816ff5d525ff2c3df6a20e24848dc8b9
Capi Etheriel <barraponto@gmail.com>
2015-11-20 00:02:12 -0200

    set up sub parsers

--- snippets.py
+++ snippets.py
@@ -23,9 +23,25 @@ def get(name):
 
 def main():
     """Main function"""
+
     logging.info("Constructing parser.")
     parser = argparse.ArgumentParser(
         description="Store and retrieve snippets of text.")
+    subparsers = parser.add_subparsers(
+        dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Store a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
+
+
     arguments = parser.parse_known_args(sys.argv[1:])
     print(arguments)
 
